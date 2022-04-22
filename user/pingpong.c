35ef38927d3045cf8e5950a0bec02aa7e6c19cf8
yyeatgrass <344588135z@gmail.com>
2022-04-22 10:12:25 +0800

    Implement pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,28 @@
+#include "kernel/types.h"
+#include <user/user.h>
+
+int
+main(int argc, char *argv[])
+{
+   char data[20];
+   int p[2];
+
+   if (pipe(p) < 0) {
+      printf("pipe failed.");
+      exit(1);
+   }
+
+   if (fork() == 0) {
+      read(p[0], data, sizeof(data));
+      printf("%d: %s\n", getpid(), data);
+      write(p[1], "pong", 5);
+      exit(0);
+   } else {
+      write(p[1], "ping", 5);
+      wait(0);
+      // read here is not blocking.
+      read(p[0], data, sizeof(data));
+      printf("%d: %s\n", getpid(), data);
+      exit(0);
+   }
+}
