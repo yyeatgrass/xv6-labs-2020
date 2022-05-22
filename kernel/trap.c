8fabe1d85ef55ff830d418478187aa3543b510a2
yyeatgrass <344588135z@gmail.com>
2022-05-22 16:45:54 +0800

    Implement lazy allocation when page fault occurs.

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,13 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if (r_scause() == 13 || r_scause() == 15) {
+    uint64 va = r_stval();
+    char *mem = kalloc();
+    memset(mem, 0, PGSIZE);
+
+    uint64 vadownboundary = PGROUNDDOWN(va);
+    mappages(p->pagetable, vadownboundary, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U);
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
@@ -82,6 +89,7 @@ usertrap(void)
 
   usertrapret();
 }
+
 
 //
 // return to user space
